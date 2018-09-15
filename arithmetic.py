059930e72851d145b85dfed90b5fbeddfa91dd5b
Hackbright Student <no-reply@hackbrightacademy.com>
2018-09-15 15:43:44 -0700

    Define modulo function

--- arithmetic.py
+++ arithmetic.py
@@ -31,6 +31,6 @@ def power(num1, num2):
     """Raise num1 to the power of num2 and return the value."""
     return num1 ** num2
 
-# def mod(num1, num2):
-#     """Return the remainder of num1 / num2."""
-#     return % num1, num2
+def mod(num1, num2):
+    """Return the remainder of num1 / num2."""
+    return num1 % num2
